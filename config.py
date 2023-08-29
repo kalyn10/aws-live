73a607b9124ec27a214301a13d2cf8c54d0a6ca4
kalyn10 <99702180+kalyn10@users.noreply.github.com>
2023-08-29 15:40:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cd9ieuxxz6h0.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chengzhilin-employee"
+customregion = "us-east-1"
